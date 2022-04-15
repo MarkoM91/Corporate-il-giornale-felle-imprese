0b38fcdc6389b823338c91e5a23af79840d0cd43
unknown <markomedic91@gmail.com>
2022-04-15 15:15:59 +0200

    'third'

--- wp-content/themes/envo-magazine/script.js
+++ wp-content/themes/envo-magazine/script.js
@@ -1,9 +1,16 @@
+// function videoScale(me) {
+//   console.log(me.index());
+//   // var meIndex = me.index();
+//   me.addClass('scale');
+// }
+
 function init() {
   $(".tag-video").closest("article").removeClass('article').remove();
   $('.article:first-of-type .post .news-item .news-thumb').removeClass('col-md-6');
   $('.article:first-of-type .post .news-item .news-thumb').addClass('col-md-12');
   $('.article:first-of-type .post .news-item .news-text-wrap').removeClass('col-md-6');
   $('.article:first-of-type .post .news-item .news-text-wrap').addClass('col-md-12');
+
 
   // <!-- Initialize Swiper -->  <script>
     var swiper = new Swiper(".mySwiper", {
@@ -15,7 +22,11 @@ function init() {
       },
     });
 
-
+   // var me = $(this).find('.swiper-slide');
+   // me.click(function() {
+   //   me
+   //   videoScale(me);
+   // });
 }
 
 $(document).ready(init);
